f0a63a60b1e9d8c5bb2b248d25c18fd2d62c9c6e
Giulliano Rossi <giulliano.scatalon.rossi@gmail.com>
2021-02-24 18:13:04 +0000

    Exemplo feito, nao vou desenvolver muito esse site, pq ja entendi como funciona

--- public/application/config/config.php
+++ public/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://8001-black-narwhal-9iu1fkxd.ws-us03.gitpod.io/';
 
 /*
 |--------------------------------------------------------------------------
